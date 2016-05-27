6a489a93ef94dc918e7dde323cbb9201b31ea369
Guillaume Jacquart <guillaume.jacquart@live.fr>
2016-05-27 16:19:27 +0200

    Http icons in push.

--- routes/chat.js
+++ routes/chat.js
@@ -32,7 +32,7 @@ function sendPush(msg, targetsToken) {
 				"android": {
 					"title": "New zoubab message",
 					"message": "user " + msg.username + " sent a new message",
-					"icon": icon,
+					"icon": "http://www.zoubab.com/images/icon.png",
 					"payload": {
 						"message": msg.msg
 					}
